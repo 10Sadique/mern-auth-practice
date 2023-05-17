b342d1827457e10efbb362ef2bdd20dfb85a5c9e
10Sadique <jafarsjahan@gmail.com>
2023-05-17 16:42:00 +0600

    react router setup implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
 
 function App() {
     return (
         <>
             <Header />
-            <HomeScreen />
+            <Container className="my-2">
+                <Outlet />
+                <ScrollRestoration />
+            </Container>
         </>
     );
 }
