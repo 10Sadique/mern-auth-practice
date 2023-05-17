117e355b3fc518aa08c15a727ca39870008351d7
10Sadique <jafarsjahan@gmail.com>
2023-05-17 12:57:08 +0600

    basic users route setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRouter.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API running");
+});
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
